a5537887fe1c06be6811f64d498280db36e3379e
SimmonsBI <benno.simmons@gmail.com>
2020-09-07 17:40:39 +0100

    Corrected test data

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -1,5 +1,6 @@
 ## code to prepare `DATASET` dataset goes here
 
+network_list <- list(matrix(1,5,5),matrix(2,10,3))
 data_CalculateRolesMultipleNetworks <- CalculateRolesMultipleNetworks(network_list = network_list, weights_method = "none")
 
 usethis::use_data(data_CalculateRolesMultipleNetworks, internal = TRUE, overwrite = TRUE)
