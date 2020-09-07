08ef9d6ab0b75af1668af202b721d02cad624791
SimmonsBI <benno.simmons@gmail.com>
2020-09-07 17:40:28 +0100

    Removed empty positions for rows and columns

--- R/CalculateRolesMultipleNetworks.R
+++ R/CalculateRolesMultipleNetworks.R
@@ -37,5 +37,13 @@ CalculateRolesMultipleNetworks <- function(network_list, ...){
   # Prepare output -----------------
   roles_rows <- do.call("rbind", roles_rows) # join row roles into a single df
   roles_columns <- do.call("rbind", roles_columns) # join column roles into a single df
+
+  position_names <- colnames(roles_rows)[grepl(pattern = "np[0-9]", x = colnames(roles_rows))] # extract column names containing position frequencies i.e. the np columns
+  position_numbers <- sapply(strsplit(position_names, split = "\\D+"), function(x) as.numeric(x[2])) # extract the position number from the np column names
+  row_position_numbers <- c(2,4,6,8,11,12,14,16,18,21,22,24,25,28,29,31,34,35,38,41,42,44,46,48,51,52,55,56,57,60,61,63,64,67,68,70,73,74,77,78,81,82,86,87,88,92,93,94,98,99,102,103,106,107,108,111,112,114,117,118,121,122,124,127,128,132,133,136,137,140,143,144,146,148) # all possible position numbers for row nodes
+
+  roles_rows <- roles_rows[,c(TRUE, TRUE, position_numbers %in% row_position_numbers)] # subset to row positions
+  roles_columns <- roles_columns[,c(TRUE, TRUE, !position_numbers %in% row_position_numbers)] # subset to column positions
+
   list(roles_rows, roles_columns) # output as list
 }
