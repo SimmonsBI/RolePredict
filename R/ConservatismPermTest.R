2781fd353c608c80bfd2855f9d1310ef115501c8
SimmonsBI <benno.simmons@gmail.com>
2020-09-14 14:24:05 +0100

    Remove strat level and network list as inputs

--- R/ConservatismPermTest.R
+++ R/ConservatismPermTest.R
@@ -1,40 +1,8 @@
-ConservatismPermTest <- function(network_list, roles, strat_level, n_it, which_taxa){
+ConservatismPermTest <- function(roles, n_it, focal_taxa){
   # Argument checks -----------------
-  # network_list
-  if(!inherits(network_list, "list")){stop("'network_list' must be a list")}
-  if(length(network_list)<2){stop("'network_list' must be a list of length 2 or more")}
-  if(!all(sapply(network_list, inherits, "matrix"))){stop("Elements of 'network_list' must be of class 'matrix'")}
-  if(!all(sapply(network_list, function(x){
-    all(apply(x, 1:2, function(y) is.numeric(y) | is.integer(y)))
-  }))){stop("Elements of 'network_list' must be matrices which only contain numeric or integer elements")}
-  if(!all(sapply(network_list, function(x){
-    all(apply(x, 1:2, function(x) length(x) == 1))
-  }))){stop("Elements of 'network_list' must be matrices whose elements are of length 1")}
-  if(!all(sapply(network_list, function(x){
-    all(apply(x, 1:2, function(x) x >= 0))
-  }))){stop("Elements of 'network_list' must be matrices whose elements are >= 0")}
-  if(!all(sapply(network_list, function(x){
-    sum(x) > 0
-  }))){stop("Elements of 'network_list' must be matrices with at least one non-zero element")}
-  for(i in seq(network_list)){
-    dn <- dimnames(network_list[[i]])
-    if(is.null(dn)){
-      stop("All matrices in 'network_list' must have row and column names i.e. taxa names")
-    } else if(any(sapply(dn, is.null))){
-      stop("All matrices in 'network_list' must have row and column names i.e. taxa names")
-    }
-  }
-  all_species <- unlist(lapply(network_list, function(x) unlist(dimnames(x))))
-  if(!any(table(all_species)>1)){stop("At least one taxon must occur more than once in the data to assess conservatism")}
-
   # roles
   if(!inherits(roles,"list")){stop("'roles' must be a list of length 2")}
   if(length(roles) != 2){stop("'roles' must be a list of length 2")}
-
-  # strat_level
-  if(!(inherits(strat_level,"numeric") | inherits(strat_level, "integer"))){stop("'strat_level' must be of class numeric or integer")}
-  if(length(strat_level) != 1){stop("'strt_level' must be of length 1")}
-  if(!strat_level %in% c(1,2,3)){stop("'strt_level' must be 1, 2 or 3")}
 
   # n_it
   if(!(inherits(n_it,"numeric") | inherits(n_it, "integer"))){stop("'n_it' must be of class numeric or integer")}
@@ -42,22 +10,46 @@ ConservatismPermTest <- function(network_list, roles, strat_level, n_it, which_t
   if(n_it < 1){stop("'n_it' must be greater than 0")}
   if(isFALSE(all.equal(n_it%%1, 0))){stop("'n_it' must be a whole number")}
 
-  # which_taxa
-  if(!inherits(which_taxa, "character")){stop("'which_taxa' must be a vector of character strings. The vector must be of length >= 1.")}
-  if(length(which_taxa) < 1){stop("'which_taxa' must be a vector of length >= 1")}
-  if(!all(which_taxa %in% all_species)){
-    species_not_in_data <- setdiff(x = unique(which_taxa), y = unique(all_species))
-    message(paste0("Error: All elements of 'which_taxa' must be present in network_list as either a row or column name. The following species are not present (these have also been returned as a vector object if you want to access these programmatically): ", paste(species_not_in_data, collapse = "; ")))
+  # focal_taxa
+  if(!inherits(focal_taxa, "character")){stop("'focal_taxa' must be a vector of character strings. The vector must be of length >= 1.")}
+  if(length(focal_taxa) < 1){stop("'focal_taxa' must be a vector of length >= 1")}
+  if(!all(table(focal_taxa)==1)){stop("focal_taxa must not contain any duplicate names")}
+
+  all_species <- c(roles[[1]]$species, roles[[2]]$species) # all occurrences of all species in 'roles'
+
+  if(focal_taxa == "all"){ # if focal_taxa == "all", assign it all species that occur more than once in 'roles'
+    focal_taxa <- names(which(table(all_species)>1))
+  }
+  if(!all(focal_taxa %in% all_species)){ # make sure all focal_taxa are in 'roles'
+    species_not_in_data <- setdiff(x = unique(focal_taxa), y = unique(all_species))
+    message(paste0("Error: All elements of 'focal_taxa' must be present in 'roles'. The following species are not present (these have also been returned as a vector object if you want to access these programmatically): ", paste(species_not_in_data, collapse = "; ")))
     return(species_not_in_data)
   }
-  if(!all(table(all_species)[which_taxa]>1)){
-    species_not_more_than_once <- names(which(!(table(all_species)[which_taxa]>1)))
-    message(paste0("Error: To assess conservatism, all elements of 'which_taxa' must be present in more than one network in network_list as a row or column name. The following species in 'which_taxa' do not occur more than once (these have also been returned as a vector object if you want to access these programmatically): ", paste(species_not_more_than_once, collapse = "; ")))
+  if(!all(table(all_species)[focal_taxa]>1)){ # make sure all focal_taxa are in 'roles' more than once
+    species_not_more_than_once <- names(which(!(table(all_species)[focal_taxa]>1)))
+    message("Error: To assess conservatism, all elements of 'focal_taxa' must occur more than once in 'roles' (it is not possible to assess conservatism across a single occurrence). Species in 'focal_taxa' that do not occur more than once have been returned as a vector object if you want to access these programmatically")
     return(species_not_more_than_once)
   }
+
+    # Make results container -----------------
+    conservatism <- data.frame(level = NA, species = NA, observed_variance = NA, mean_null_variance = NA, delta = NA, z = NA, P = rep(NA, length(focal_taxa)))
+
+    # Calculate conservatism -----------------
+    for(taxon in focal_taxa){
+      if(taxon %in% roles[[1]]$species){
+
+      } else if(taxon %in% roles[[2]]$species){
+      # roles_taxon <- roles[roles$speces]
+    }
+
+
+    }
 }
 
-
-
-
-
+library(nettools)
+m1 <- default_names(rbm(10,10,0.3))
+m2 <- default_names(rbm(10,10,0.3))
+roles <- CalculateRolesMultipleNetworks(network_list = list(m1,m2), weights_method = "none")
+focal_taxa <- c("r1","r2")
+strat_level <- 1
+n_it <- 10
