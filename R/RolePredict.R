fae7c3622f6c38543f34f4929c26681de1031576
SimmonsBI <benno.simmons@gmail.com>
2020-09-18 12:55:37 +0100

    Refining argument checks

--- R/RolePredict.R
+++ R/RolePredict.R
@@ -1,4 +1,4 @@
-RolePredict <- function(training_networks, networks_to_predict, conservatism, weighting = NULL, n_it, species_remapping, ...){
+RolePredict <- function(training_networks, networks_to_predict, conservatism, weighting = NULL, n_it, species_remapping = NULL, ...){
   # Argument checks -----------------
   # training networks
   if(any(sapply(training_networks, function(x){any(is.null(rownames(x)), is.null(colnames(x)))}))){stop("All elements of training_networks must have row and column names")}
@@ -6,14 +6,17 @@ RolePredict <- function(training_networks, networks_to_predict, conservatism, we
   training_roles <- CalculateRolesMultipleNetworks(network_list = training_networks, ...) # calculate roles of all species in training_networks
 
   # species_remapping
-  if(!(inherits(species_remapping, "data.frame"))){stop("'species_remapping' must be a data.frame")}
-  if(ncol(species_remapping) != 2){stop("'species_remapping' must be a data.frame with 2 columns: 'species' and 'proxy'")}
-  if(!identical(colnames(species_remapping), c("species","proxy"))){stop("'species_remapping' must be a data.frame with 2 columns: 'species' and 'proxy'")}
-  if(!inherits(species_remapping$species, "character")){stop("The species column in 'species_remapping' must be a character variable")}
-  if(!inherits(species_remapping$proxy, "character")){stop("The proxy column in 'species_remapping' must be a character variable")}
   ntp_species <- unlist(lapply(networks_to_predict, function(x) x$species))
-  if(!all(species_remapping$species %in% networks_to_predict)){stop("All species in 'species' column of species_remapping must be present in networks_to_predict")}
-  if(!all(species_remapping$proxy %in% all_species)){stop("All species in 'proxy' column of species_remapping must be present in training_networks")}
+  if(!is.null(species_remapping)){
+    if(!(inherits(species_remapping, "data.frame"))){stop("'species_remapping' must be a data.frame")}
+    if(ncol(species_remapping) != 2){stop("'species_remapping' must be a data.frame with 2 columns: 'species' and 'proxy'")}
+    if(!identical(colnames(species_remapping), c("species","proxy"))){stop("'species_remapping' must be a data.frame with 2 columns: 'species' and 'proxy'")}
+    if(!inherits(species_remapping$species, "character")){stop("The species column in 'species_remapping' must be a character variable")}
+    if(!inherits(species_remapping$proxy, "character")){stop("The proxy column in 'species_remapping' must be a character variable")}
+    if(!all(species_remapping$species %in% ntp_species)){stop("All species in 'species' column of species_remapping must be present in networks_to_predict")}
+    if(!all(species_remapping$proxy %in% all_species)){stop("All species in 'proxy' column of species_remapping must be present in training_networks")}
+    if(!all(table(species_remapping$species)==1)){stop("Species must only have one proxy. This error appears because one or more species occurs more than once in the 'species' column of species_remapping")}
+  }
 
   # networks_to_predict
   if(!all(sapply(networks_to_predict, function(x) inherits(x, "data.frame")))){stop("All elements of networks_to_predict must be a data.frame")}
@@ -22,17 +25,19 @@ RolePredict <- function(training_networks, networks_to_predict, conservatism, we
   if(!all(sapply(networks_to_predict, function(x) all(x$level %in% c("row","column"))))){stop("All elements of networks_to_predict must have a column called 'level' whose entries are either 'row' or 'column'")}
   if(!all(sapply(networks_to_predict, function(x) inherits(x$species, "character")))){stop("All elements of networks_to_predict must have a column called 'species' whose entries are character strings giving species names. Currently, elements of one or more 'species' columns are not of class 'chaarcter'.")}
   if(!all(sapply(networks_to_predict, function(x) all(table(x$level)>=4)))){stop("All elements of networks_to_predict must have 4 or more row species and 4 or more column species")}
-  networks_to_predict_remapping <- networks_to_predict # do remapping for proxy species in networks_to_predict
-  for(i in seq(networks_to_predict_remapping)){
-    x <- networks_to_predict_remapping[[i]]
-    x$species <- sapply(x$species, function(y){
-      if(y %in% species_remapping$species){
-        species_remapping[species_remapping$species == y,"proxy"]
-      } else {
-        y
-      }
-    })
-    networks_to_predict_remapping[[i]] <- x
+  networks_to_predict_remapping <- networks_to_predict # create networks_to_predict_remapping even if there is no remapping, so there is a unified variable name for the subsequent checks
+  if(!is.null(species_remapping)){ # do remapping for proxy species in networks_to_predict if any is specified
+    for(i in seq(networks_to_predict_remapping)){
+      x <- networks_to_predict_remapping[[i]]
+      x$species <- sapply(x$species, function(y){
+        if(y %in% species_remapping$species){
+          species_remapping[species_remapping$species == y,"proxy"]
+        } else {
+          y
+        }
+      })
+      networks_to_predict_remapping[[i]] <- x
+    }
   }
   if(!all(sapply(networks_to_predict_remapping, function(x){all(x$species %in% all_species)}))){stop("Not all species in networks_to_predict are in training_networks, even after performing any remapping specified using species_remapping. All species in networks_to_predict must be present in training_networks or be replaced by a proxy that is in training_networks using the species_remapping argument")}
   if(!all(sapply(networks_to_predict_remapping, function(x){
@@ -41,7 +46,7 @@ RolePredict <- function(training_networks, networks_to_predict, conservatism, we
 
   # conservatism
   if(!inherits(conservatism, "character")){stop("'conservatism' must be a character string equal to: 'none','conservatism_output', 'calculate', 'custom'")}
-  if(!length(conservatism) != 1){stop("'conservatism' must be a character string of length 1 equal to: 'none','conservatism_output', 'calculate', 'custom'")}
+  if(length(conservatism) != 1){stop("'conservatism' must be a character string of length 1 equal to: 'none','conservatism_output', 'calculate', 'custom'")}
   if(!conservatism %in% c("none","conservatism_output", "calculate", "custom")){stop("'conservatism' must be a character string equal to: 'none','conservatism_output', 'calculate', 'custom'")}
 
   # weighting
@@ -60,9 +65,11 @@ RolePredict <- function(training_networks, networks_to_predict, conservatism, we
     if(any(weighting$P > 1)){stop("'weighting' must be the output of Conservatism() or ConservatismPermTest(): values of column P are > 1")}
     if(any(weighting$P < 0)){stop("'weighting' must be the output of Conservatism() or ConservatismPermTest(): values of column P are < 0")}
     if(!all(weighting$species %in% all_species)){stop("'weighting' must be the output of Conservatism() or ConservatismPermTest() run on the same data as in training_networks. Not all species in 'weighting' are present in training_networks")}
+    #note to self:  add check that all species that occur > once in training data are in weights if using conservatism output
   } else if(conservatism == "calculate"){
     if(!is.null(weighting)){stop("If 'conservatism' == 'calculate', you cannot set the 'weighting' argument, it must be left blank")}
   } else if(conservatism == "custom"){
+    if(is.null(weighting)){stop("weighting is NULL. Data must be provided for the 'weighting' argument when conservatism = 'custom'.")}
     if(!inherits(weighting, "data.frame")){stop("'weighting' must be a data.frame")}
     if(ncol(weighting) != 3){stop("'weighting' must have 3 columns: level, species, and weight")}
     if(!identical(colnames(weighting), c("level","species","weight"))){stop("'weighting' must have 3 columns: level, species, and weight")}
@@ -70,6 +77,16 @@ RolePredict <- function(training_networks, networks_to_predict, conservatism, we
     if(!inherits(weighting$weight, "numeric")){stop("The column 'weight' must be numeric")}
     if(any(weighting$weight < 0)){stop("All weighting in the weight column must be positive numbers")}
     if(!all(weighting$species %in% all_species)){stop("All species in 'weighting' must be present in training_networks")}
+    if(is.null(species_remapping)){
+      if(!all(ntp_species %in% weighting$species)){stop("All species in 'networks_to_predict' must have weights specified in 'weighting' when using conservatism = 'custom'")}
+    } else { # must have weights for all ntp spp w/ no proxies; all ntp spp w/ proxies which occur more than once in t_n; and all proxy species
+      if(!all(setdiff(ntp_species, species_remapping$species) %in% weighting$species)){stop("All species in networks_to_predict that do not have proxies specified via species_remapping must have weighted assigned to them in 'weighting'")}
+      if(!all(species_remapping$proxy %in% weighting$species)){stop("All proxy species in species_remapping must have weights assigned to them in 'weighting'")}
+      species_remapping_species_frequency <- sapply(species_remapping$species, function(x) sum(all_species == x))
+      if(any(species_remapping_species_frequency>1)){
+        if(!all(names(which(species_remapping_species_frequency > 1)) %in% weighting$species)){stop("All species in the 'species' column of species_remapping must have weights assigned to them in 'weighting' if they occur more than once in training_networks. This is because for species occurring more than once in training_networks, RolePredict can calculate their conservatism and average these values with the conservatism values of the proxy species.")}
+      }
+    }
   }
 
   # checking networks_to_predict row and column assignments match training_networks
@@ -80,20 +97,24 @@ RolePredict <- function(training_networks, networks_to_predict, conservatism, we
   if(!all(networks_to_predict_remapping_rbind$level == networks_to_predict_remapping_rbind$training_species_levels)){stop("The level of species  (row or column) in networks_to_predict, after any remapping from species_remapping, must match whether these species are row or column names in training_networks. e.g. a species specified as 'row' in networks_to_predict, directly or via remapping, must also be a row name, not a column name, in training_networks")}
 
   # checking species and proxy row and column assignments match in species_remapping
-  networks_to_predict_rbind <- do.call("rbind", networks_to_predict)
-  species_species_remapping_levels <- networks_to_predict_rbind[match(species_remapping$species, networks_to_predict_rbind$species),"level"] # level of species in species_remapping
-  proxy_species_remapping_levels <- training_species_levels[match(species_remapping$proxy, training_species_levels$species),"level"] # level or proxy in species_remapping
-  if(!all(species_species_remapping_levels == proxy_species_remapping_levels)){stop("In species_remapping, species in the 'species' column must be in the same level (row or column) as determined by networks_to_predict as the corresponding species in the 'proxy' column")}
+  if(!is.null(species_remapping)){
+    networks_to_predict_rbind <- do.call("rbind", networks_to_predict)
+    species_species_remapping_levels <- networks_to_predict_rbind[match(species_remapping$species, networks_to_predict_rbind$species),"level"] # level of species in species_remapping
+    proxy_species_remapping_levels <- training_species_levels[match(species_remapping$proxy, training_species_levels$species),"level"] # level or proxy in species_remapping
+    if(!all(species_species_remapping_levels == proxy_species_remapping_levels)){stop("In species_remapping, species in the 'species' column must be in the same level (row or column) as determined by networks_to_predict as the corresponding species in the 'proxy' column")}
+  }
 
   # checking row/column assignments in weighting match training_networks
-  species_weightings_levels <- training_species_levels[match(weighting$species, training_species_levels$species),"level"] # level of species in weightings in training_networks
-  if(!all(species_weightings_levels == weighting$level)){stop("The level (row/column) of species in the 'species' column of 'weightings' must match must match whether these species are row or column names in training_networks. e.g. a species specified as 'row' in weightings must also be a row name, not a column name, in training_networks")}
+  if(conservatism %in% c("conservatism_output","custom")){
+    species_weightings_levels <- training_species_levels[match(weighting$species, training_species_levels$species),"level"] # level of species in weightings in training_networks
+    if(!all(species_weightings_levels == weighting$level)){stop("The level (row/column) of species in the 'species' column of 'weightings' must match must match whether these species are row or column names in training_networks. e.g. a species specified as 'row' in weightings must also be a row name, not a column name, in training_networks")}
+  }
 
+  # Calculate conservatism -----------------
+  if(conservatism == "calculate"){
+    # weightings <- ConservatismPermTest(roles = training_roles, n_it = n_it, species = wanted_predictions)
+  }
 
-
-
-
-  conservatism <- ConservatismPermTest(roles = roles, n_it = n_it, species = wanted_predictions)
 
 
 
@@ -102,4 +123,12 @@ RolePredict <- function(training_networks, networks_to_predict, conservatism, we
 
 
 
-networks_to_predict <- list(data.frame(level = c("row","row","row","row","column","column","column","column"), species = c("Erythroxylum","Paragenipa","Dillenia","Mimusops","C1","C11","C15","C19")))
+
+
+
+
+
+
+
+
+
